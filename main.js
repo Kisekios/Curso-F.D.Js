58cf62e85aec5abbadcc231bb169c530f6f6ed01
Kisekios <dekostump@gmail.com>
2024-01-15 22:07:30 -0500

    La seccion Aside de detail, cuando se selecciona una imagen ahora muestra la imagen y los detalles de esta

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+/* Llamada a los items de HTML */
+
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
@@ -7,15 +9,41 @@ const aside = document.querySelector('.product-detail');
 const cardsContainer = document.querySelector('.cards-container');
 const productDetailContainer = document.querySelector('.detail');
 const closeProductDetail = document.querySelector('.product-detail-close');
+const imgSelect = document.getElementsByClassName('product-card');
+const imgdetail = document.getElementById('imgdetail');
+const priceContainerAside = document.getElementById('price');
+const namePictureContainerAside = document.getElementById('namePicture');
+const detailProductContainerAside = document.getElementById('detailProduct');
+
+
+/* Variables Globales */
 
 
 const productList = [];
+
+
+/* Lectura de eventos en HTML */
 
 
 navEmail.addEventListener('click', toggleDesktopMenu);
 menuHamIcon.addEventListener('click', toggleMobileMenu);
 menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 closeProductDetail.addEventListener('click', closeDetails);
+
+function renderAsideDetail() {
+    for (elemento of imgSelect) {
+        elemento.addEventListener('click', function () {
+            let nameOfPictureClick
+            nameOfPictureClick = this.getAttribute('id')
+            const selectedProduct = productList.find(product => product.name === nameOfPictureClick);
+            renderInfoDetailAside(selectedProduct);
+
+        })
+    }
+}
+
+
+/* Funciones para mostrar/ocultar los items del HTML */
 
 
 function toggleDesktopMenu() {
@@ -48,11 +76,69 @@ function openProductDetail() {
     aside.classList.add('inactive');
 
     productDetailContainer.classList.remove('inactive');
+    renderAsideDetail();
 }
 
 function closeDetails() {
     productDetailContainer.classList.add('inactive');
 }
+
+
+/* Redenderizar a HTML */
+
+
+function renderProducts(arreglo) {
+    for (product of arreglo) {
+        const productCard = document.createElement('div');
+        productCard.classList.add('product-card');
+        productCard.id = product.name
+
+        const img = document.createElement('img');
+        img.setAttribute('src', product.image);
+        img.addEventListener('click', openProductDetail);
+
+        const productInfo = document.createElement('div');
+        productInfo.classList.add('product-info');
+
+        const productInfoDiv = document.createElement('div');
+
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + product.price;
+        const productName = document.createElement('p');
+        productName.innerText = product.name;
+
+        productInfoDiv.appendChild(productPrice);
+        productInfoDiv.appendChild(productName);
+
+        const productInfoFigure = document.createElement('figure');
+        const productImgCard = document.createElement('img');
+        productImgCard.setAttribute('src', './icons/bt_add_to_cart.svg');
+
+        productInfoFigure.appendChild(productImgCard);
+
+        productInfo.appendChild(productInfoDiv);
+        productInfo.appendChild(productInfoFigure);
+
+        productCard.appendChild(img);
+        productCard.appendChild(productInfo);
+
+        cardsContainer.appendChild(productCard);
+    }
+
+}
+
+function renderInfoDetailAside(producto) {
+    imgdetail.src = producto.image;
+    imgdetail.alt = producto.name;
+    priceContainerAside.innerText = '$' + producto.price;
+    namePictureContainerAside.innerText = producto.name;
+    detailProductContainerAside.innerText = "Imagenes de series animadas";
+
+    console.log(producto)
+}
+
+
+/* Listado de productos */
 
 
 productList.push({
@@ -91,58 +177,16 @@ productList.push({
     image: "https://i.pinimg.com/564x/7c/c8/52/7cc852fc55c4bde9a3b6643280390f6e.jpg"
 });
 
+
+/* Llamada a las funciones */
+
+
 renderProducts(productList);
 
 
-function renderProducts(arreglo) {
-    for (product of arreglo) {
-        const productCard = document.createElement('div');
-        productCard.classList.add('product-card');
 
-        const img = document.createElement('img');
-        img.setAttribute('src', product.image);
-        img.addEventListener('click', openProductDetail);
 
-        const productInfo = document.createElement('div');
-        productInfo.classList.add('product-info');
 
-        const productInfoDiv = document.createElement('div');
-
-        const productPrice = document.createElement('p');
-        productPrice.innerText = '$' + product.price;
-        const productName = document.createElement('p');
-        productName.innerText = product.name;
-
-        productInfoDiv.appendChild(productPrice);
-        productInfoDiv.appendChild(productName);
-
-        const productInfoFigure = document.createElement('figure');
-        const productImgCard = document.createElement('img');
-        productImgCard.setAttribute('src', './icons/bt_add_to_cart.svg');
-
-        productInfoFigure.appendChild(productImgCard);
-
-        productInfo.appendChild(productInfoDiv);
-        productInfo.appendChild(productInfoFigure);
-
-        productCard.appendChild(img);
-        productCard.appendChild(productInfo);
-
-        cardsContainer.appendChild(productCard);
-    }
-    /* <!-- <div class="product-card">
-                <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
-                <div class="product-info">
-                    <div>
-                        <p>$120,00</p>
-                        <p>Bike</p>
-                    </div>
-                    <figure>
-                        <img src="./icons/bt_add_to_cart.svg" alt="">
-                    </figure>
-                </div>
-    </div> --> */
-}
 
 
 
